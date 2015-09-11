087d662195ef6a8be0473b9c839e3c82b0c33443
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-09-11 16:19:20 +0300

    remove SuppressWarnings

--- src/main/java/com/ibm/watson/developer_cloud/util/ResponseUtil.java
+++ src/main/java/com/ibm/watson/developer_cloud/util/ResponseUtil.java
@@ -171,7 +171,6 @@ public class ResponseUtil {
 	public static <T> T getObject(HttpResponse response, Class<T> type) throws IOException {
 		String jsonString = getString(response);
 		
-		@SuppressWarnings("unchecked")
 		T pojo = (T) GsonSingleton.getGson().fromJson(jsonString, type);
 		return pojo;
 	}
