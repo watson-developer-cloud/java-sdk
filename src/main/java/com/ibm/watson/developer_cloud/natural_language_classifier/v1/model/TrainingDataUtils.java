3aee04214146fd14fae4577c3d463312de192f2e
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-09-12 18:27:59 +0300

    Added apache.commons.lang to the wrappers and remove google collections references.

--- src/main/java/com/ibm/watson/developer_cloud/natural_language_classifier/v1/model/TrainingDataUtils.java
+++ src/main/java/com/ibm/watson/developer_cloud/natural_language_classifier/v1/model/TrainingDataUtils.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,7 +15,6 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
-import com.google.common.collect.Lists;
 
 /**
  * Utility class to transform training data to CSV
@@ -43,7 +43,7 @@ public class TrainingDataUtils {
 						|| trainingData.getClasses().isEmpty())
 					log.log(Level.WARNING, trainingData + " couldn't be converted to a csv record");
 				else {
-					List<String> record = Lists.newArrayList(trainingData.getText());
+					List<String> record = Arrays.asList(trainingData.getText());
 					record.addAll(trainingData.getClasses());
 					printer.printRecord(record.toArray());
 				}
