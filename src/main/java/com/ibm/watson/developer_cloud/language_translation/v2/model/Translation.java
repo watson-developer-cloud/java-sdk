6fbcb078675644c189cde8be6ab11323f8f6db92
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-09-15 10:04:27 -0400

    language translation use GenericModel now

--- src/main/java/com/ibm/watson/developer_cloud/language_translation/v2/model/Translation.java
+++ src/main/java/com/ibm/watson/developer_cloud/language_translation/v2/model/Translation.java
@@ -2,14 +2,14 @@ package com.ibm.watson.developer_cloud.language_translation.v2.model;
 
 import com.google.gson.annotations.Expose;
 import com.ibm.watson.developer_cloud.language_translation.v2.LanguageTranslation;
-import com.ibm.watson.developer_cloud.util.GsonSingleton;
+import com.ibm.watson.developer_cloud.service.model.GenericModel;
 
 /**
  * The Translation result used as POJO by the {@link LanguageTranslation}.
- *
+ * 
  * @author German Attanasio Ruiz (germanatt@us.ibm.com)
  */
-public class Translation {
+public class Translation extends GenericModel {
 
 	/** The translation. */
 	@Expose
@@ -17,7 +17,7 @@ public class Translation {
 
 	/**
 	 * Gets the translation.
-	 *
+	 * 
 	 * @return The translation
 	 */
 	public String getTranslation() {
@@ -26,34 +26,11 @@ public class Translation {
 
 	/**
 	 * Sets the translation.
-	 *
+	 * 
 	 * @param translation
 	 *            The translation
 	 */
 	public void setTranslation(final String translation) {
 		this.translation = translation;
-	}
-
-	/**
-	 * With translation.
-	 *
-	 * @param translation
-	 *            the translation
-	 * @return the translation
-	 */
-	public Translation withTranslation(final String translation) {
-		this.translation = translation;
-		return this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return getClass().getName() + " "
-				+ GsonSingleton.getGson().toJson(this);
 	}
 }
