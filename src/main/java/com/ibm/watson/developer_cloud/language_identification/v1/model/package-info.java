06ae9967de697a37c690480793a65f714efa4e85
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-10-13 16:38:29 -0400

    remove machine translation and language identification #31

--- src/main/java/com/ibm/watson/developer_cloud/language_identification/v1/model/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Language Identification POJOs
- */
-package com.ibm.watson.developer_cloud.language_identification.v1.model;
-
--- src/main/java/com/ibm/watson/developer_cloud/machine_translation/v1/model/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Machine Translation POJOs
- */
-package com.ibm.watson.developer_cloud.machine_translation.v1.model;
-
