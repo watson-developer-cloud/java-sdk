7dc9340ce6c25a6c4409d63ac755ef12d959eb99
Nizar <nmalsedd@us.ibm.com>
2015-10-12 22:02:11 -0500

    fixed annotateText method
    changed IDHelper.getGraphId(graph, getAccountId());
    to
    String graphId = IDHelper.getGraphId(graph, getAccountId());

--- src/main/java/com/ibm/watson/developer_cloud/concept_insights/v2/ConceptInsights.java
+++ src/main/java/com/ibm/watson/developer_cloud/concept_insights/v2/ConceptInsights.java
@@ -217,10 +217,10 @@ public class ConceptInsights extends WatsonService {
 	 * @return {@link Annotations}
 	 */
 	public Annotations annotateText(final Graph graph, final String text) {
-		IDHelper.getGraphId(graph, getAccountId());
+		String graphId = IDHelper.getGraphId(graph, getAccountId());
 		Validate.notNull(text, "text can't be null");
 
-		HttpRequestBase request = Request.Post(API_VERSION + graph.getId() + ANNOTATE_TEXT_PATH)
+		HttpRequestBase request = Request.Post(API_VERSION + graphId + ANNOTATE_TEXT_PATH)
 				.withContent(text, MediaType.TEXT_PLAIN).withHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
 				.build();
 
