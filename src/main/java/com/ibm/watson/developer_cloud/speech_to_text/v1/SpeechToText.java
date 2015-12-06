73806f090a7935b910cb1f4499e615970a4a2872
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-12-05 20:01:58 -0500

    [STT] Improve error message when calling recognize #133

--- src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/SpeechToText.java
+++ src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/SpeechToText.java
@@ -228,13 +228,13 @@ public class SpeechToText extends WatsonService {
    * @return the {@link SpeechResults}
    */
   public SpeechResults recognize(File audio, String contentType, RecognizeOptions options) {
-    Validate.isTrue(audio != null && audio.exists(), "audio is not a valid audio file");
+    Validate.isTrue(audio != null && audio.exists(), "audio file is null or does not exist");
 
     Validate.isTrue((audio.length() / (1024 * 1024)) < 100.0,
         "The audio file is greater than 100MB.");
 
-    Validate
-        .isTrue(MediaType.parse(contentType) != null, "contentType is not a valid audio format");
+    Validate.isTrue(MediaType.parse(contentType) != null,
+        "contentType is not a valid mime audio format. Valid formats starts with 'audio/'");
 
     String path = PATH_RECOGNIZE;
     if (options != null && (options.getSessionId() != null && !options.getSessionId().isEmpty()))
