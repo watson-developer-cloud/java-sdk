4e8d87cf27803cc754cfe3e8aaec7f2871494a3c
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-12-05 20:03:28 -0500

    fix misspell

--- src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/SpeechToText.java
+++ src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/SpeechToText.java
@@ -229,12 +229,13 @@ public class SpeechToText extends WatsonService {
    */
   public SpeechResults recognize(File audio, String contentType, RecognizeOptions options) {
     Validate.isTrue(audio != null && audio.exists(), "audio file is null or does not exist");
+    Validate.isTrue(audio != null && audio.exists(), "audio file is null or does not exist");
 
     Validate.isTrue((audio.length() / (1024 * 1024)) < 100.0,
         "The audio file is greater than 100MB.");
 
     Validate.isTrue(MediaType.parse(contentType) != null,
-        "contentType is not a valid mime audio format. Valid formats starts with 'audio/'");
+        "contentType is not a valid mime audio format. Valid formats start with 'audio/'");
 
     String path = PATH_RECOGNIZE;
     if (options != null && (options.getSessionId() != null && !options.getSessionId().isEmpty()))
