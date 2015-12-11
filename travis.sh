152096dead4bb54d006916412a4972d32b7b490f
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-12-11 00:15:00 -0500

    updated coverage maven plugin

--- travis.sh
+++ travis.sh
@@ -2,9 +2,11 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	echo '$TRAVIS_PULL_REQUEST is false, running all tests'
-	mvn clean install -Dmaven.test.skip=true && mvn clean verify cobertura:cobertura-integration-test coveralls:report
+	mvn clean install -Dmaven.test.skip=true
+	mvn clean verify cobertura:cobertura-integration-test coveralls:report
   codecov
 else
 	echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running unit tests'
-	mvn clean install -Dmaven.test.skip=true && mvn clean test
+	mvn clean install -Dmaven.test.skip=true
+	mvn clean test
 fi
