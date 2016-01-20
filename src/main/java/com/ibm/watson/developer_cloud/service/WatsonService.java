4af269dafb701af6ab5cc9f8c2e8559cc13e69ae
jaayers <jaayers@us.ibm.com>
2016-01-20 11:25:53 -0500

    Add support for jndi API key lookup

--- src/main/java/com/ibm/watson/developer_cloud/service/WatsonService.java
+++ src/main/java/com/ibm/watson/developer_cloud/service/WatsonService.java
@@ -28,7 +28,7 @@ import com.ibm.watson.developer_cloud.http.HttpHeaders;
 import com.ibm.watson.developer_cloud.http.HttpStatus;
 import com.ibm.watson.developer_cloud.http.RequestBuilder;
 import com.ibm.watson.developer_cloud.service.model.GenericModel;
-import com.ibm.watson.developer_cloud.util.BluemixUtils;
+import com.ibm.watson.developer_cloud.util.CredentialUtils;
 import com.ibm.watson.developer_cloud.util.RequestUtil;
 import com.ibm.watson.developer_cloud.util.ResponseUtil;
 import com.squareup.okhttp.Credentials;
@@ -65,7 +65,7 @@ public abstract class WatsonService {
    */
   public WatsonService(String name) {
     this.name = name;
-    this.apiKey = BluemixUtils.getAPIKey(name);
+    this.apiKey = CredentialUtils.getAPIKey(name);
     this.client = configureHttpClient();
   }
 
