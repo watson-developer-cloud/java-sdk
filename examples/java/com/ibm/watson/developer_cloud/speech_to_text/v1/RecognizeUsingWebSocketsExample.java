d31e3cf455536890ee13307300ea7b777ca751bb
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-01-21 23:30:21 -0500

    Added codecov

--- examples/java/com/ibm/watson/developer_cloud/speech_to_text/v1/RecognizeUsingWebSocketsExample.java
+++ examples/java/com/ibm/watson/developer_cloud/speech_to_text/v1/RecognizeUsingWebSocketsExample.java
@@ -12,7 +12,7 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeD
 /**
  * Recognize using WebSockets a sample wav file and print the transcript into the console output.
  */
-public class RecognizeUsingWebSockets {
+public class RecognizeUsingWebSocketsExample {
   private static CountDownLatch lock = new CountDownLatch(1);
 
   public static void main(String[] args) throws FileNotFoundException, InterruptedException {
