093ee3f30a5a9bcaad3c69843d5d2b284f6ca3a9
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-02-11 16:59:00 -0500

    switch back to cobertura

--- .utility/travis.sh
+++ .utility/travis.sh
@@ -2,10 +2,8 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	echo '$TRAVIS_PULL_REQUEST is false, running all tests'
-	mvn clean integration-test
+	mvn clean cobertura:cobertura-integration-test
 else
 	echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running unit tests'
 	mvn clean test
 fi
-# merge the unit test and integration test reports
-mvn jacoco:merge
