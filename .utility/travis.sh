af5c7c60275ebaae603d9c10df5e0582d1969b8b
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-02-11 16:13:12 -0500

    merge unit test and integration test results

--- .utility/travis.sh
+++ .utility/travis.sh
@@ -2,8 +2,10 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	echo '$TRAVIS_PULL_REQUEST is false, running all tests'
-	mvn clean cobertura:cobertura-integration-test coveralls:report
+	mvn clean integration-test
 else
 	echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running unit tests'
 	mvn clean test
 fi
+# merge the unit test and integration test reports
+mvn jacoco:merge
