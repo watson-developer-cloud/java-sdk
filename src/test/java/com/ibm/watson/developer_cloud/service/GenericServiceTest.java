54fff5db42da2897d00e6387d8c20f9a7d38aee0
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-03-18 13:39:59 -0400

    [ci skip] bump up version to 2.9.0

--- src/test/java/com/ibm/watson/developer_cloud/service/GenericServiceTest.java
+++ src/test/java/com/ibm/watson/developer_cloud/service/GenericServiceTest.java
@@ -179,7 +179,7 @@ public class GenericServiceTest extends WatsonServiceUnitTest {
     mockAPICall();
     service.getProfile(sampleText);
     mockServer.verify(new HttpRequest().withMethod("POST").withHeader(
-        new Header(HttpHeaders.USER_AGENT, "watson-developer-cloud-java-sdk-2.8.0")));
+        new Header(HttpHeaders.USER_AGENT, "watson-developer-cloud-java-sdk-2.9.0")));
   }
 
   @Test
