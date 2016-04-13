52b283090d8ceb20581d774267fb1df5db8c7f59
tanmayb123 <tajymany@gmail.com>
2016-04-13 13:52:51 -0400

    Update Language.java

--- src/main/java/com/ibm/watson/developer_cloud/language_translation/v2/model/Language.java
+++ src/main/java/com/ibm/watson/developer_cloud/language_translation/v2/model/Language.java
@@ -2,6 +2,23 @@ package com.ibm.watson.developer_cloud.language_translation.v2.model;
 
 public enum Language {
 
-  ARABIC, ENGLISH, SPANISH, FRENCH, ITALIAN, PORTUGESE
+  ARABIC, ENGLISH, SPANISH, FRENCH, ITALIAN, PORTUGESE,
+  AR, EN, ES, FR, IT, PT
+  
+  @Override
+  public String toString() {
+    if (super.toString().length() > 2) {
+      switch (this) {
+        case ARABIC: return "ar";
+        case ENGLISH: return "en";
+        case SPANISH: return "es";
+        case FRENCH: return "fr";
+        case ITALIAN: return "it";
+        case PORTUGESE: return "pt";
+      }
+    } else {
+      return super.toString();
+    }
+  }
 
 }
