e596d6576d845ce14a973ed52ee61c29b6124685
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-04-22 12:57:06 -0400

    Added documentation to each package #250

---
+++ src/main/java/com/ibm/watson/developer_cloud/alchemy/v1/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides access to the Alchemy Language, Vision and DataNews services
+ */
+package com.ibm.watson.developer_cloud.alchemy.v1;
+
---
+++ src/main/java/com/ibm/watson/developer_cloud/http/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains interfaces, enumerations and implementations to work with HTTP calls
+ */
+package com.ibm.watson.developer_cloud.http;
+
