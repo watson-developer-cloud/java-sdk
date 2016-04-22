f779ffb6a2ec34ef1d5670c4851dc3e57b2d58e3
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-04-22 11:55:31 -0400

    Deploy snapshots into sonatype #243

--- .utility/travis.sh
+++ .utility/travis.sh
@@ -2,7 +2,8 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	echo '$TRAVIS_PULL_REQUEST is false, running all tests'
-	mvn clean cobertura:cobertura-integration-test
+  openssl aes-256-cbc -K $encrypted_a973fe4f8e79_key -iv $encrypted_a973fe4f8e79_iv -in config.properties.enc -out src/test/resources/config.properties -d
+  mvn clean cobertura:cobertura-integration-test
 else
 	echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running unit tests'
 	mvn clean test
