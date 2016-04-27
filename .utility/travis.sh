af829f02eb88545fc7151af166b73ddd0035ac25
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-04-27 09:54:47 -1000

    Updated alchemy key

--- .utility/travis.sh
+++ .utility/travis.sh
@@ -2,7 +2,7 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	echo '$TRAVIS_PULL_REQUEST is false, running all tests'
-  openssl aes-256-cbc -K $encrypted_a973fe4f8e79_key -iv $encrypted_a973fe4f8e79_iv -in config.properties.enc -out src/test/resources/config.properties -d
+  openssl aes-256-cbc -K $encrypted_a973fe4f8e79_key -iv $encrypted_a973fe4f8e79_iv -in .config.properties.enc -out src/test/resources/.config.properties -d
   mvn clean cobertura:cobertura-integration-test
 else
 	echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running unit tests'
