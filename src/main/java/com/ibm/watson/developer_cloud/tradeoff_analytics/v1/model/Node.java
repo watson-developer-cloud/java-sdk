353388bddd97a97aebcde2b2c817ce3f251a247a
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-05-17 21:38:30 -0400

    fixes #261 adding map to the resolution

---
+++ src/main/java/com/ibm/watson/developer_cloud/tradeoff_analytics/v1/model/Node.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.ibm.watson.developer_cloud.tradeoff_analytics.v1.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+
+
+/**
+ * The Class Node.
+ */
+public class Node {
+
+    private Position coordinates;
+
+    @SerializedName("solution_refs")
+    private List<String> solutionRefs = new ArrayList<String>();
+
+    /**
+     * Gets the coordinates.
+     *
+     * @return the coordinates
+     */
+    public Position getCoordinates() {
+        return coordinates;
+    }
+
+    /**
+     * Gets the solution refs.
+     *
+     * @return the solution refs
+     */
+    public List<String> getSolutionRefs() {
+        return solutionRefs;
+    }
+
+    /**
+     * Sets the coordinates.
+     *
+     * @param coordinates the new coordinates
+     */
+    public void setCoordinates(Position coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * Sets the solution refs.
+     *
+     * @param solutionRefs the new solution refs
+     */
+    public void setSolutionRefs(List<String> solutionRefs) {
+        this.solutionRefs = solutionRefs;
+    }
+
+}
---
+++ src/main/java/com/ibm/watson/developer_cloud/tradeoff_analytics/v1/model/ResolutionConfig.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.ibm.watson.developer_cloud.tradeoff_analytics.v1.model;
+
+import java.util.Map;
+
+/**
+ * Resolution config.
+ */
+public class ResolutionConfig {
+    private Map<String,String> drivers;
+    private Map<String,String> params;
+    
+    /**
+     * Gets the drivers.
+     *
+     * @return the drivers
+     */
+    public Map<String, String> getDrivers() {
+      return drivers;
+    }
+    
+    /**
+     * Gets the params.
+     *
+     * @return the params
+     */
+    public Map<String, String> getParams() {
+      return params;
+    }
+    
+    /**
+     * Sets the drivers.
+     *
+     * @param drivers the drivers
+     */
+    public void setDrivers(Map<String, String> drivers) {
+      this.drivers = drivers;
+    }
+    
+    /**
+     * Sets the params.
+     *
+     * @param params the params
+     */
+    public void setParams(Map<String, String> params) {
+      this.params = params;
+    }
+
+}
