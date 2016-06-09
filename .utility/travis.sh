c98d6d7c1a7a4ae3ca2ced393f6a30c8f5f5eda1
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-06-09 19:13:28 -0400

    generate javadoc for commits

--- .utility/travis.sh
+++ .utility/travis.sh
@@ -2,6 +2,8 @@
 
 set -e # enforces the script to fail as soon as one command fails
 
+mvn javadoc:javadoc > /dev/null
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   echo '$TRAVIS_PULL_REQUEST is false, running all tests'
   openssl aes-256-cbc -K $encrypted_a973fe4f8e79_key -iv $encrypted_a973fe4f8e79_iv -in .config.properties.enc -out src/test/resources/.config.properties -d
