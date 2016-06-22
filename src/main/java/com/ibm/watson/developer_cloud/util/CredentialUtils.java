571b7782e2d8d7411d5827b9db00841b0fae6314
Dan O'Connor <dan_o'connor@us.ibm.com>
2016-06-21 20:31:31 -0400

    Updates based on PR comments

--- src/main/java/com/ibm/watson/developer_cloud/util/CredentialUtils.java
+++ src/main/java/com/ibm/watson/developer_cloud/util/CredentialUtils.java
@@ -40,7 +40,7 @@ public final class CredentialUtils {
     private String password;
     private String username;
 
-    public ServiceCredentials(String username, String password) {
+    private ServiceCredentials(String username, String password) {
       this.username = username;
       this.password = password;
     }
@@ -51,14 +51,6 @@ public final class CredentialUtils {
 
     public String getUsername() {
       return username;
-    }
-
-    public void setPassword(String password) {
-      this.password = password;
-    }
-
-    public void setUsername(String username) {
-      this.username = username;
     }
   }
 
