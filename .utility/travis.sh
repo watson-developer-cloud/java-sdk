bc829ccf1ae3b9037de9efb3347d71d843bb0ae2
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-09-29 00:34:19 -0400

    fail if checkstyle fail

--- .utility/travis.sh
+++ .utility/travis.sh
@@ -2,6 +2,9 @@
 
 set -e # enforces the script to fail as soon as one command fails
 
+mvn checkstyle:checkstyle
+mvn javadoc:aggregate > /dev/null
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   echo '$TRAVIS_PULL_REQUEST is false, running all tests'
   mvn clean cobertura:cobertura-integration-test
