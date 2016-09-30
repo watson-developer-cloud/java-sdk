3d26f7d9294358fe4627571cb872df99613ce0be
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-09-30 13:22:58 -0400

    :new: Adds Speech to Text callbacks

---
+++ speech-to-text/src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/model/RecognitionCallback.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.ibm.watson.developer_cloud.speech_to_text.v1.model;
+
+import com.ibm.watson.developer_cloud.service.model.GenericModel;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+
+/**
+ * Used in {@link SpeechToText#registerCallback(String, String)} to register a callback URL to be notified by the
+ * service of the recognition status and, optionally, the results automatically.
+ */
+public class RecognitionCallback extends GenericModel {
+  private String status;
+  private String url;
+
+  /**
+   * Gets the status.
+   *
+   * @return the status
+   */
+  public String getStatus() {
+    return status;
+  }
+
+  /**
+   * Sets the status.
+   *
+   * @param status the new status
+   */
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  /**
+   * Gets the url.
+   *
+   * @return the url
+   */
+  public String getUrl() {
+    return url;
+  }
+
+  /**
+   * Sets the url.
+   *
+   * @param url the new url
+   */
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+}
