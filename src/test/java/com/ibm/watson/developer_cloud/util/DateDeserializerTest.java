9a21f864f17497bcb5fdecfe04d2adb80845b4ed
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2016-09-30 17:11:23 -0400

    fixed line length problems

--- src/test/java/com/ibm/watson/developer_cloud/util/DateDeserializerTest.java
+++ src/test/java/com/ibm/watson/developer_cloud/util/DateDeserializerTest.java
@@ -2,6 +2,8 @@ package com.ibm.watson.developer_cloud.util;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.sun.tools.javac.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,7 +12,13 @@ public class DateDeserializerTest {
 
     @Test
     public void testDeserialize() {
-        String jsonStr = "[\"2016-06-20T04:25:16.218+0000\", \"2016-06-20T04:25:16\", \"2016-06-20T04:25:16.218Z\",\"2015-05-28T18:01:57Z\",\"2016-06-20T04:25:16.218+0000\"]";
+        String[] dateStrings = {"2016-06-20T04:25:16.218+0000",
+                "2016-06-20T04:25:16",
+                "2016-06-20T04:25:16.218Z",
+                "2015-05-28T18:01:57Z",
+                "2016-06-20T04:25:16.218+0000"};
+
+        String jsonStr = "[\"" + StringUtils.join(dateStrings,"\",\"") + "\"]";
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(jsonStr);
         DateDeserializer deserializer = new DateDeserializer();
