1477976d98791de4e9ad72d4c204bce3b5762a02
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2016-11-02 08:57:13 -0400

    Deals with a situation where epoch seconds
    It appears that sometimes epoch seconds are used in dates instead of
    a normal string date.  The date deserialize was modified to include
    the ability to accept both epoch seconds and epoch milliseconds as
    well as normal dates.

--- core/src/main/java/com/ibm/watson/developer_cloud/util/DateDeserializer.java
+++ core/src/main/java/com/ibm/watson/developer_cloud/util/DateDeserializer.java
@@ -20,6 +20,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
@@ -74,6 +76,20 @@ public class DateDeserializer implements JsonDeserializer<Date> {
       } catch (ParseException e1) {
         e = e1;
       }
+    }
+
+    Pattern isJustNumber = Pattern.compile("^\\d+$");
+    Matcher foundMatch = isJustNumber.matcher(dateAsString);
+    if (foundMatch.find()) {
+      Long time_t = Long.parseLong(dateAsString);
+      Long msCheck = 1000000000000L;
+
+      // are we ms or seconds maybe?
+      if (time_t > msCheck) {
+        // assuming milliseconds
+        time_t = time_t / 1000;
+      }
+      return new Date(time_t);
     }
 
     LOG.log(Level.SEVERE, "Error parsing: " + dateAsString, e);
