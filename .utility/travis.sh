990a8bd6a6a98b39e48bfc73d2ba166075567a17
Martin Harvan <martinhrvn@gmail.com>
2016-11-18 19:55:18 +0100

    Changng to use gradle wrapper in travis.sh

--- .utility/travis.sh
+++ .utility/travis.sh
@@ -2,14 +2,14 @@
 
 set -e # enforces the script to fail as soon as one command fails
 
-gradle install -x check
+../gradlew install -x check
 gradle docs > /dev/null
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   echo '$TRAVIS_PULL_REQUEST is false, running all tests'
   openssl aes-256-cbc -K $encrypted_a973fe4f8e79_key -iv $encrypted_a973fe4f8e79_iv -in .config.properties.enc -out tests/src/test/resources/.config.properties -d
-  gradle cobertura
+  ../gradlew cobertura
 else
   echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running unit tests'
-  gradle check
+  ../gradlew check
 fi
