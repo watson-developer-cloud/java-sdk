94266afbd842ab29f148be68b025c25d4ac4d5b6
Blake Ball <bhball@us.ibm.com>
2017-01-20 14:55:59 -0600

    correcting fully qualified paths

--- .utility/travis.sh
+++ .utility/travis.sh
@@ -7,7 +7,7 @@ set -e # enforces the script to fail as soon as one command fails
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   echo '$TRAVIS_PULL_REQUEST is false, running all tests'
-  openssl aes-256-cbc -K $encrypted_a973fe4f8e79_key -iv $encrypted_a973fe4f8e79_iv -in .config.properties.enc -out tests/src/test/resources/.config.properties -d
+  openssl aes-256-cbc -K $encrypted_a973fe4f8e79_key -iv $encrypted_a973fe4f8e79_iv -in .config.properties.enc -out core/src/test/resources/.config.properties -d
   ./gradlew cobertura
 else
   echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running unit tests'
