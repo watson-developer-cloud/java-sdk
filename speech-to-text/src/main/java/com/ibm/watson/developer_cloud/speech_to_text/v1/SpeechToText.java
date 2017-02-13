cf8a6ab914e85fe47637f1393f1ea44e03924253
Jeff Kaminski <jpkamins@us.ibm.com>
2017-02-13 15:42:22 -0500

    :fire Remove extranneous code for overloaded Speech to Text getWords() method

--- speech-to-text/src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/SpeechToText.java
+++ speech-to-text/src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/SpeechToText.java
@@ -661,14 +661,7 @@ public class SpeechToText extends WatsonService {
    * @return the words
    */
   public ServiceCall<List<WordData>> getWords(String customizationId, Word.Type type) {
-    Validator.notNull(customizationId, "customizationId cannot be null");
-    RequestBuilder requestBuilder = RequestBuilder.get(String.format(PATH_WORDS, customizationId));
-    if (type != null) {
-      requestBuilder.query(WORD_TYPE, type.toString().toLowerCase());
-    }
-
-    ResponseConverter<List<WordData>> converter = ResponseConverterUtils.getGenericObject(TYPE_WORDS, WORDS);
-    return createServiceCall(requestBuilder.build(), converter);
+    return getWords(customizationId, type, null);
   }
 
   /**
