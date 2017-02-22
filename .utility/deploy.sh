9816ced1e5976b21660d6148c4fb92e4424a6f26
Blake Ball <bhball@us.ibm.com>
2017-02-21 19:10:45 -0600

    countering pesky ghost travis gradle assemble

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+#if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_bdb9d73a7940_key -iv $encrypted_bdb9d73a7940_iv -in cd/secring.gpg.enc -out cd/secring.gpg -d
     ls -l cd
     ./gradlew signJars -Psigning.keyId=$SIGNING_KEY -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=$KEYRING_PATH -PossrhUsername=$OSSRH_JIRA_USERNAME -PossrhPassword=$OSSRH_JIRA_PASSWORD
-fi
+#fi
