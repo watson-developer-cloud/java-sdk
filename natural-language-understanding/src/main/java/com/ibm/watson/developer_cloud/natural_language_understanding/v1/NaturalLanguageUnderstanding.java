6b90a729ef18133760e27b3218959f70076e7370
Mike Kistler <mkistler@us.ibm.com>
2017-03-08 08:38:02 -0600

    Text flow app description

--- natural-language-understanding/src/main/java/com/ibm/watson/developer_cloud/natural_language_understanding/v1/NaturalLanguageUnderstanding.java
+++ natural-language-understanding/src/main/java/com/ibm/watson/developer_cloud/natural_language_understanding/v1/NaturalLanguageUnderstanding.java
@@ -28,7 +28,50 @@ import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.In
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.ListModelsResults;
 
 /**
- * Analyze various features of text content at scale. Provide text, raw HTML, or a public URL, and IBM Watson Natural Language Understanding will give you results for the features you request. The service cleans HTML content before analysis by default, so the results can ignore most advertisements and other unwanted content.    ### Concepts  Identify general concepts that are referenced or alluded to in your content. Concepts that are detected typically have an associated link to a DBpedia resource.    ### Entities  Detect important people, places, geopolitical entities and other types of entities in your content. Entity detection recognizes consecutive coreferences of each entity. For example, analysis of the following text would count \"Barack Obama\" and \"He\" as the same entity:    \"Barack Obama was the 44th President of the United States. He took office in January 2009.\"    ### Keywords  Determine the most important keywords in your content. Keyword phrases are organized by relevance in the results.    ### Categories  Categorize your content into a hierarchical 5-level taxonomy. For example, \"Leonardo DiCaprio won an Oscar\" returns \"/art and entertainment/movies and tv/movies\" as the most confident classification.    ### Sentiment  Determine whether your content conveys postive or negative sentiment. Sentiment information can be returned for detected entities, keywords, or user-specified target phrases found in the text.    ### Emotion  Detect anger, disgust, fear, joy, or sadness that is conveyed by your content. Emotion information can be returned for detected entities, keywords, or user-specified target phrases found in the text.    ### Relations  Recognize when two entities are related, and identify the type of relation.  For example, you can identify an \"awardedTo\" relation between an award and its recipient.    ### Semantic Roles  Parse sentences into subject-action-object form, and identify entities and keywords that are subjects or objects of an action.    ### Metadata  Get author information, publication date, and the title of your text/HTML content.  
+ * Analyze various features of text content at scale. Provide text, raw HTML, or a public URL, and
+ * IBM Watson Natural Language Understanding will give you results for the features you request. The
+ * service cleans HTML content before analysis by default, so the results can ignore most
+ * advertisements and other unwanted content.
+ *
+ * ### Concepts
+ * Identify general concepts that are referenced or alluded to in your content. Concepts that are
+ * detected typically have an associated link to a DBpedia resource.
+ *
+ * ### Entities
+ * Detect important people, places, geopolitical entities and other types of entities in your
+ * content. Entity detection recognizes consecutive coreferences of each entity. For example,
+ * analysis of the following text would count "Barack Obama" and "He" as the same entity:
+ *
+ * "Barack Obama was the 44th President of the United States. He took office in January 2009."
+ *
+ * ### Keywords
+ * Determine the most important keywords in your content. Keyword phrases are organized by relevance
+ * in the results.
+ *
+ * ### Categories
+ * Categorize your content into a hierarchical 5-level taxonomy. For example, "Leonardo DiCaprio won
+ * an Oscar" returns "/art and entertainment/movies and tv/movies" as the most confident
+ * classification.
+ *
+ * ### Sentiment
+ * Determine whether your content conveys postive or negative sentiment. Sentiment information can
+ * be returned for detected entities, keywords, or user-specified target phrases found in the text.
+ *
+ * ### Emotion
+ * Detect anger, disgust, fear, joy, or sadness that is conveyed by your content. Emotion
+ * information can be returned for detected entities, keywords, or user-specified target phrases
+ * found in the text.
+ *
+ * ### Relations
+ * Recognize when two entities are related, and identify the type of relation.  For example, you can
+ * identify an "awardedTo" relation between an award and its recipient.
+ *
+ * ### Semantic Roles
+ * Parse sentences into subject-action-object form, and identify entities and keywords that are
+ * subjects or objects of an action.
+ *
+ * ### Metadata
+ * Get author information, publication date, and the title of your text/HTML content.
  *
  * @version v1
  * @see <a href="http://www.ibm.com/watson/developercloud/natural-language-understanding.html">Natural Language Understanding</a>
