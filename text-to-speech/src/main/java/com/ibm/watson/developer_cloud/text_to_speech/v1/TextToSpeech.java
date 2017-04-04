19948d53f3ad93ac7241ba67fccac8d023bea63e
Blake Ball <bhball@us.ibm.com>
2017-04-04 17:59:14 -0500

    :bug: escape semicolon prior to sending to server

--- text-to-speech/src/main/java/com/ibm/watson/developer_cloud/text_to_speech/v1/TextToSpeech.java
+++ text-to-speech/src/main/java/com/ibm/watson/developer_cloud/text_to_speech/v1/TextToSpeech.java
@@ -216,8 +216,9 @@ public class TextToSpeech extends WatsonService {
     Validator.isTrue((text != null) && !text.isEmpty(), "text cannot be null or empty");
     Validator.isTrue(voice != null, "voice cannot be null or empty");
 
+    String modifiedText = text.replace(";", "%3B");
     final RequestBuilder request = RequestBuilder.get(PATH_SYNTHESIZE);
-    request.query(TEXT, text);
+    request.query(TEXT, modifiedText);
     request.query(VOICE, voice.getName());
     request.query(ACCEPT, audioFormat != null ? audioFormat : AudioFormat.WAV);
 
