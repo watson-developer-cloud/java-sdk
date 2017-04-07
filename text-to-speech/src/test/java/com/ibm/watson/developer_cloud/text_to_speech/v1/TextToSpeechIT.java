e438d87c533758882fb39e7570e47f486700fd36
German Attanasio Ruiz <germanattanasio@gmail.com>
2017-04-07 17:04:07 -0400

    TTS Synthesize using POST, fixes #635 and #602

--- text-to-speech/src/test/java/com/ibm/watson/developer_cloud/text_to_speech/v1/TextToSpeechIT.java
+++ text-to-speech/src/test/java/com/ibm/watson/developer_cloud/text_to_speech/v1/TextToSpeechIT.java
@@ -102,7 +102,7 @@ public class TextToSpeechIT extends WatsonServiceTest {
    */
   @Test
   public void testSynthesize() throws IOException {
-    String text = "This is an integration test";
+    String text = "This is an integration test; 1,2 !, @, #, $, %, ^, 20.";
     InputStream result = service.synthesize(text, Voice.EN_LISA, AudioFormat.WAV).execute();
     writeInputStreamToFile(result, File.createTempFile("tts-audio", "wav"));
   }
