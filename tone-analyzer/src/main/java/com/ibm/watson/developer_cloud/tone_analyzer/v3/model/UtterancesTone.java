7a5fcfe15b7fa635a868c5749c62f749e934534a
Shubhanjan Shekhar <shekhar@us.ibm.com>
2017-04-10 17:42:59 -0700

    :new: Added support for Customer Care API endpoint for Tone Analyzer

---
+++ tone-analyzer/src/main/java/com/ibm/watson/developer_cloud/tone_analyzer/v3/model/UtterancesTone.java
@@ -0,0 +1,34 @@
+package com.ibm.watson.developer_cloud.tone_analyzer.v3.model;
+
+import com.google.gson.annotations.SerializedName;
+import com.ibm.watson.developer_cloud.service.model.GenericModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main object containing the result of running the Tone Analyzer Chat Analysis on a conversation. It contains a list
+ * of utterance-level analysis results.
+ */
+public class UtterancesTone extends GenericModel {
+
+  @SerializedName("utterancesTone")
+  List<UtteranceAnalysis> utterancesTone = new ArrayList<>();
+
+  /**
+   * Gets the utterances tone.
+   * @return the utterances tone
+   */
+  public List<UtteranceAnalysis> getUtterancesTone() {
+    return utterancesTone;
+  }
+
+  /**
+   * Sets the utterances tone.
+   * @param utterancesTone the new utterances tone
+   */
+  public void setUtterancesTone(List<UtteranceAnalysis> utterancesTone) {
+    this.utterancesTone = utterancesTone;
+  }
+
+}
