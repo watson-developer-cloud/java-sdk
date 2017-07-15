52e54b1c0304c441617131f21c64cd529b795d8c
ExtremoBlando <r.martinsanta@alumnos.urjc.es>
2017-07-15 14:13:06 +0200

    Styling
    + in a new line

--- core/src/main/java/com/ibm/watson/developer_cloud/service/WatsonService.java
+++ core/src/main/java/com/ibm/watson/developer_cloud/service/WatsonService.java
@@ -407,8 +407,8 @@ public abstract class WatsonService {
       case HttpStatus.BAD_REQUEST: // HTTP 400
         throw new BadRequestException(error != null ? error : "Bad Request", response);
       case HttpStatus.UNAUTHORIZED: // HTTP 401
-        throw new UnauthorizedException("Unauthorized: Access is denied due to invalid credentials. " +
-                                        "Tip: Did you set the Endpoint?", response);
+        throw new UnauthorizedException("Unauthorized: Access is denied due to invalid credentials. "
+                                        + "Tip: Did you set the Endpoint?", response);
       case HttpStatus.FORBIDDEN: // HTTP 403
         throw new ForbiddenException(error != null ? error : "Forbidden: Service refuse the request", response);
       case HttpStatus.NOT_FOUND: // HTTP 404
