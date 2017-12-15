4ab4391f7bca2882f7778fd8f25f68c797af34b7
Logan Patino <loganpatino10@gmail.com>
2017-12-15 15:38:44 -0500

    :green_heart: Fix build error with java docs

--- discovery/src/main/java/com/ibm/watson/developer_cloud/discovery/v1/model/NluEnrichmentEmotion.java
+++ discovery/src/main/java/com/ibm/watson/developer_cloud/discovery/v1/model/NluEnrichmentEmotion.java
@@ -54,9 +54,9 @@ public class NluEnrichmentEmotion extends GenericModel {
     }
 
     /**
-     * Adds an targets to targets.
+     * Adds a target to targets.
      *
-     * @param targets the new targets
+     * @param target the new target
      * @return the NluEnrichmentEmotion builder
      */
     public Builder addTarget(String target) {
