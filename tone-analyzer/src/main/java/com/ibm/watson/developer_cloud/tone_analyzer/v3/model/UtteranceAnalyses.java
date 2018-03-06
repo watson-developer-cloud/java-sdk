395e4f4bb27dcc7b2c0f77280531882ec67b3165
Logan Patino <loganpatino10@gmail.com>
2018-03-06 13:47:29 -0500

    feat(tone-analyzer): Add newest generated code

--- tone-analyzer/src/main/java/com/ibm/watson/developer_cloud/tone_analyzer/v3/model/UtteranceAnalyses.java
+++ tone-analyzer/src/main/java/com/ibm/watson/developer_cloud/tone_analyzer/v3/model/UtteranceAnalyses.java
@@ -48,22 +48,4 @@ public class UtteranceAnalyses extends GenericModel {
   public String getWarning() {
     return warning;
   }
-
-  /**
-   * Sets the utterancesTone.
-   *
-   * @param utterancesTone the new utterancesTone
-   */
-  public void setUtterancesTone(final List<UtteranceAnalysis> utterancesTone) {
-    this.utterancesTone = utterancesTone;
-  }
-
-  /**
-   * Sets the warning.
-   *
-   * @param warning the new warning
-   */
-  public void setWarning(final String warning) {
-    this.warning = warning;
-  }
 }
