bbb3cfc09e99dd05b6bd20f4d5ff2a25cbb7fc07
Logan Patino <loganpatino10@gmail.com>
2018-03-30 16:38:33 -0400

    refactor(examples): Update Visual Recognition example

--- examples/src/main/java/com/ibm/watson/developer_cloud/visual_recognition/v3/VisualRecognitionExample.java
+++ examples/src/main/java/com/ibm/watson/developer_cloud/visual_recognition/v3/VisualRecognitionExample.java
@@ -14,42 +14,52 @@ package com.ibm.watson.developer_cloud.visual_recognition.v3;
 
 import java.io.File;
 
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifierOptions;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifier;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.Classifier;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.CreateClassifierOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.UpdateClassifierOptions;
 
 public class VisualRecognitionExample {
 
   public static void main(String[] args) {
-    VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
+    VisualRecognition service = new VisualRecognition("2016-05-20");
     service.setApiKey("<api-key>");
 
     System.out.println("Classify an image");
-    ClassifyImagesOptions options =
-        new ClassifyImagesOptions.Builder().images(new File("src/test/resources/visual_recognition/car.png")).build();
-    VisualClassification result = service.classify(options).execute();
+    ClassifyOptions options = new ClassifyOptions.Builder()
+        .imagesFile(new File("src/test/resources/visual_recognition/car.png"))
+        .imagesFilename("car.png")
+        .build();
+    ClassifiedImages result = service.classify(options).execute();
     System.out.println(result);
 
 
     System.out.println("Create a classifier with positive and negative images");
-    ClassifierOptions createOptions = new ClassifierOptions.Builder().classifierName("foo")
+    CreateClassifierOptions createOptions = new CreateClassifierOptions.Builder()
+        .name("foo")
         .addClass("car", new File("src/test/resources/visual_recognition/car_positive.zip"))
         .addClass("baseball", new File("src/test/resources/visual_recognition/baseball_positive.zip"))
-        .negativeExamples(new File("src/test/resources/visual_recognition/negative.zip")).build();
-    VisualClassifier foo = service.createClassifier(createOptions).execute();
+        .negativeExamples(new File("src/test/resources/visual_recognition/negative.zip"))
+        .build();
+    Classifier foo = service.createClassifier(createOptions).execute();
     System.out.println(foo);
 
     System.out.println("Classify using the 'Car' classifier");
-    options = new ClassifyImagesOptions.Builder().images(new File("src/test/resources/visual_recognition/car.png"))
-        .classifierIds("car").build();
+    options = new ClassifyOptions.Builder()
+        .imagesFile(new File("src/test/resources/visual_recognition/car.png"))
+        .imagesFilename("car.png")
+        .addClassifierId(foo.getClassifierId())
+        .build();
     result = service.classify(options).execute();
     System.out.println(result);
 
     System.out.println("Update a classifier with more positive images");
-    ClassifierOptions updateOptions = new ClassifierOptions.Builder()
-        .addClass("car", new File("src/test/resources/visual_recognition/car_positive.zip")).build();
-    VisualClassifier updatedFoo = service.updateClassifier(foo.getId(), updateOptions).execute();
+    UpdateClassifierOptions updateOptions = new UpdateClassifierOptions.Builder()
+        .classifierId(foo.getClassifierId())
+        .addClass("car", new File("src/test/resources/visual_recognition/car_positive.zip"))
+        .build();
+    Classifier updatedFoo = service.updateClassifier(updateOptions).execute();
     System.out.println(updatedFoo);
 
   }
