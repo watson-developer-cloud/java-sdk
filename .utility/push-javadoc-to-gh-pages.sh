a8bc7179b12124f365b2a9cef56a64e6fb2ed9cd
chohanbin <mraerok@gmail.com>
2018-04-23 15:23:14 -0400

    :unamused: Update places where apidiff.sh used to be invoked to call generate-api-diff.sh instead.

--- .utility/push-javadoc-to-gh-pages.sh
+++ .utility/push-javadoc-to-gh-pages.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" ]; then
     ../.utility/generate_index_html.sh > index.html
 
     # produce API diff of the current version and the latest release
-    ../.utility/apidiff.sh
+    ../.utility/generate-api-diff.sh
     rm -rf apidiff/
     mkdir apidiff/
     cp -f ../build/reports/java-sdk-api-diff* apidiff/
