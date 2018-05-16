9cddae69204e56c4ce8517221df033e7ef53afd9
Logan Patino <loganpatino10@gmail.com>
2018-05-16 13:17:14 -0400

    feat(assistant): Add manual tweaks

--- assistant/src/main/java/com/ibm/watson/developer_cloud/assistant/v1/Assistant.java
+++ assistant/src/main/java/com/ibm/watson/developer_cloud/assistant/v1/Assistant.java
@@ -82,6 +82,7 @@ import com.ibm.watson.developer_cloud.assistant.v1.model.WorkspaceExport;
 import com.ibm.watson.developer_cloud.http.RequestBuilder;
 import com.ibm.watson.developer_cloud.http.ServiceCall;
 import com.ibm.watson.developer_cloud.service.WatsonService;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
 import com.ibm.watson.developer_cloud.util.GsonSingleton;
 import com.ibm.watson.developer_cloud.util.ResponseConverterUtils;
 import com.ibm.watson.developer_cloud.util.Validator;
@@ -128,6 +129,20 @@ public class Assistant extends WatsonService {
   public Assistant(String versionDate, String username, String password) {
     this(versionDate);
     setUsernameAndPassword(username, password);
+  }
+
+  /**
+   * Instantiates a new `Assistant` with IAM. Note that if the access token is specified in the iamOptions,
+   * you accept responsibility for managing the access token yourself. You must set a new access token before this one
+   * expires. Failing to do so will result in authentication errors after this token expires.
+   *
+   * @param versionDate The version date (yyyy-MM-dd) of the REST API to use. Specifying this value will keep your API
+   *          calls from failing when the service introduces breaking changes.
+   * @param iamOptions the options for authenticating through IAM
+   */
+  public Assistant(String versionDate, IamOptions iamOptions) {
+    this(versionDate);
+    setIamCredentials(iamOptions);
   }
 
   /**
