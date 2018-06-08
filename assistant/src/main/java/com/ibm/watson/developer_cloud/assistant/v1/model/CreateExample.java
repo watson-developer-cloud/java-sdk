45748b82de5fb5f27b22f3385ca8ae17e76dc255
Logan Patino <loganpatino10@gmail.com>
2018-06-08 16:18:12 -0400

    feat(assistant): Add generated updates

--- assistant/src/main/java/com/ibm/watson/developer_cloud/assistant/v1/model/CreateExample.java
+++ assistant/src/main/java/com/ibm/watson/developer_cloud/assistant/v1/model/CreateExample.java
@@ -85,9 +85,10 @@ public class CreateExample extends GenericModel {
   /**
    * Gets the text.
    *
-   * The text of a user input example. This string must conform to the following restrictions: - It cannot contain
-   * carriage return, newline, or tab characters. - It cannot consist of only whitespace characters. - It must be no
-   * longer than 1024 characters.
+   * The text of a user input example. This string must conform to the following restrictions:
+   * - It cannot contain carriage return, newline, or tab characters.
+   * - It cannot consist of only whitespace characters.
+   * - It must be no longer than 1024 characters.
    *
    * @return the text
    */
--- assistant/src/main/java/com/ibm/watson/developer_cloud/assistant/v1/model/CreateIntent.java
+++ assistant/src/main/java/com/ibm/watson/developer_cloud/assistant/v1/model/CreateIntent.java
@@ -134,9 +134,10 @@ public class CreateIntent extends GenericModel {
   /**
    * Gets the intent.
    *
-   * The name of the intent. This string must conform to the following restrictions: - It can contain only Unicode
-   * alphanumeric, underscore, hyphen, and dot characters. - It cannot begin with the reserved prefix `sys-`. - It must
-   * be no longer than 128 characters.
+   * The name of the intent. This string must conform to the following restrictions:
+   * - It can contain only Unicode alphanumeric, underscore, hyphen, and dot characters.
+   * - It cannot begin with the reserved prefix `sys-`.
+   * - It must be no longer than 128 characters.
    *
    * @return the intent
    */
