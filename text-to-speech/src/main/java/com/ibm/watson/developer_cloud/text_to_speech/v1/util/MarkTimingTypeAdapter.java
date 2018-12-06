dd6b5a1179d8005e56f3e32c5bf8a08a4f13cd34
Logan Patino <loganpatino10@gmail.com>
2018-12-06 12:18:24 -0500

    feat(Text to Speech): Initial commit of code for synthesizeUsingWebSocket

---
+++ text-to-speech/src/main/java/com/ibm/watson/developer_cloud/text_to_speech/v1/util/MarkTimingTypeAdapter.java
@@ -0,0 +1,51 @@
+package com.ibm.watson.developer_cloud.text_to_speech.v1.util;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.MarkTiming;
+
+import java.io.IOException;
+
+public class MarkTimingTypeAdapter extends TypeAdapter<MarkTiming> {
+  /*
+   * (non-Javadoc)
+   * @see com.google.gson.TypeAdapter#read(com.google.gson.stream.JsonReader)
+   */
+  @Override
+  public MarkTiming read(JsonReader in) throws IOException {
+    if (in.peek() == JsonToken.NULL) {
+      in.nextNull();
+      return null;
+    }
+
+    final MarkTiming markTiming = new MarkTiming();
+    in.beginArray();
+
+    if (in.peek() == JsonToken.STRING) {
+      markTiming.setMark(in.nextString());
+    }
+    if (in.peek() == JsonToken.NUMBER) {
+      markTiming.setTime(in.nextDouble());
+    }
+
+    in.endArray();
+    return markTiming;
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see com.google.gson.TypeAdapter#write(com.google.gson.stream.JsonWriter, java.lang.Object)
+   */
+  @Override
+  public void write(JsonWriter out, MarkTiming markTiming) throws IOException {
+    out.beginArray();
+
+    out.value(markTiming.getMark());
+    out.value(markTiming.getTime());
+
+    out.endArray();
+    out.flush();
+  }
+}
---
+++ text-to-speech/src/main/java/com/ibm/watson/developer_cloud/text_to_speech/v1/websocket/TextToSpeechWebSocketListener.java
@@ -0,0 +1,176 @@
+package com.ibm.watson.developer_cloud.text_to_speech.v1.websocket;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Marks;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.SynthesizeOptions;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Timings;
+import com.ibm.watson.developer_cloud.util.GsonSingleton;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TextToSpeechWebSocketListener extends WebSocketListener {
+  private static final String TEXT_TO_WEB_SOCKET = "TextToWebSocketThread";
+  private static final Gson GSON = GsonSingleton.getGsonWithoutPrettyPrinting();
+  private static final Logger LOG = Logger.getLogger(TextToSpeechWebSocketListener.class.getName());
+
+  private static final String VOICE = "voice";
+  private static final String CUSTOMIZATION_ID = "customization_id";
+  private static final String ACTION = "action";
+  private static final String START = "start";
+  private static final String STOP = "stop";
+  private static final String ERROR = "error";
+  private static final String WARNINGS = "warnings";
+  private static final String BINARY_STREAMS = "binary_streams";
+  private static final String CONTENT_TYPE = "content_type";
+  private static final String WORDS = "words";
+  private static final String MARKS = "marks";
+
+
+  private final SynthesizeOptions options;
+  private final SynthesizeCallback callback;
+  private WebSocket socket;
+  private boolean socketOpen = true;
+
+  public TextToSpeechWebSocketListener(final SynthesizeOptions options, final SynthesizeCallback callback) {
+    this.options = options;
+    this.callback = callback;
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see okhttp3.WebSocketListener#onClosing(okhttp3.WebSocket, int, java.lang.String)
+   */
+  @Override
+  public void onClosing(WebSocket webSocket, int code, String reason) {
+    socketOpen = false;
+    callback.onDisconnected();
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see okhttp3.WebSocketListener#onFailure(okhttp3.WebSocket, java.lang.Throwable, okhttp3.Response)
+   */
+  @Override
+  public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+    socketOpen = false;
+    if (t instanceof Exception) {
+      callback.onError((Exception) t);
+    } else {
+      callback.onError(new Exception(t));
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see okhttp3.WebSocketListener#onMessage(okhttp3.WebSocket, java.lang.String)
+   */
+  @Override
+  public void onMessage(WebSocket webSocket, String message) {
+    JsonObject json = new JsonParser().parse(message).getAsJsonObject();
+    if (json.has(ERROR)) {
+      String error = json.get(ERROR).getAsString();
+      callback.onError(new RuntimeException(error));
+
+    } else if (json.has(WARNINGS)) {
+      String warning = json.get(WARNINGS).getAsString();
+      callback.onWarning(new RuntimeException(warning));
+    } else if (json.has(BINARY_STREAMS)) {
+      String contentType = json.get(BINARY_STREAMS)
+          .getAsJsonArray()
+          .get(0)
+          .getAsJsonObject()
+          .get(CONTENT_TYPE)
+          .getAsString();
+      callback.onContentType(contentType);
+    } else if (json.has(WORDS)) {
+      callback.onTimings(GSON.fromJson(message, Timings.class));
+    } else if (json.has(MARKS)) {
+      callback.onMarks(GSON.fromJson(message, Marks.class));
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see okhttp3.WebSocketListener#onMessage(okhttp3.WebSocket, okio.ByteString)
+   */
+  @Override
+  public void onMessage(WebSocket webSocket, ByteString bytes) {
+    callback.onAudioStream(bytes.toByteArray());
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see okhttp3.WebSocketListener#onOpen(okhttp3.WebSocket, okhttp3.Response)
+   */
+  @Override
+  public void onOpen(final WebSocket webSocket, Response response) {
+    callback.onConnected();
+    this.socket = webSocket;
+    if (!this.socket.send(buildStartMessage(this.options))) {
+      callback.onError(new IOException("WebSocket unavailable"));
+    } else {
+      new Thread(TEXT_TO_WEB_SOCKET) {
+        @Override
+        public void run() {
+          sendText();
+          if (socketOpen && !socket.send(buildStopMessage())) {
+            LOG.log(Level.SEVERE, "Stop message discarded because WebSocket is unavailable");
+          }
+        }
+      };
+    }
+  }
+
+  private void sendText() {
+    try {
+      while (socketOpen) {
+        socket.send(this.options.text());
+      }
+    } catch (Exception e) {
+      LOG.log(Level.SEVERE, e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Builds the start message.
+   *
+   * @param options the options
+   * @return the request
+   */
+  private String buildStartMessage(SynthesizeOptions options) {
+    Gson gson = new GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .create();
+    JsonObject startMessage = new JsonParser()
+        .parse(gson.toJson(options))
+        .getAsJsonObject();
+
+    // remove options that are already in query string
+    startMessage.remove(VOICE);
+    startMessage.remove(CUSTOMIZATION_ID);
+
+    startMessage.addProperty(ACTION, START);
+    return startMessage.toString();
+  }
+
+  /**
+   * Builds the stop message.
+   *
+   * @return the string
+   */
+  private String buildStopMessage() {
+    JsonObject stopMessage = new JsonObject();
+    stopMessage.addProperty(ACTION, STOP);
+    return stopMessage.toString();
+  }
+}
