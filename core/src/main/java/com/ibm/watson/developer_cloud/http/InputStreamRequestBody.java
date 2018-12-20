41a83a329e38fa475d479b54ae8b1edec769e56d
Logan Patino <loganpatino10@gmail.com>
2018-12-20 08:51:17 -0500

    fix(core): Allow stream to be reused when logging all

--- core/src/main/java/com/ibm/watson/developer_cloud/http/InputStreamRequestBody.java
+++ core/src/main/java/com/ibm/watson/developer_cloud/http/InputStreamRequestBody.java
@@ -12,15 +12,20 @@
  */
 package com.ibm.watson.developer_cloud.http;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import com.ibm.watson.developer_cloud.util.HttpLogging;
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okhttp3.internal.Util;
+import okhttp3.logging.HttpLoggingInterceptor;
 import okio.BufferedSink;
 import okio.Okio;
 import okio.Source;
+import org.apache.commons.io.IOUtils;
 
 /**
  * RequestBody that takes an {@link InputStream}.
@@ -30,6 +35,7 @@ public class InputStreamRequestBody extends RequestBody {
 
   private InputStream inputStream;
   private MediaType mediaType;
+  private byte[] bytes;
 
   /**
    * Creates the @link {@link RequestBody} from an @link {@link InputStream}.
@@ -45,6 +51,19 @@ public class InputStreamRequestBody extends RequestBody {
   private InputStreamRequestBody(InputStream inputStream, MediaType mediaType) {
     this.inputStream = inputStream;
     this.mediaType = mediaType;
+
+    // if we're logging everything, we'll need to store the bytes to reuse later
+    if (HttpLogging.getLoggingInterceptor().getLevel().equals(HttpLoggingInterceptor.Level.BODY)) {
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+      try {
+        IOUtils.copy(inputStream, outputStream);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      this.bytes = outputStream.toByteArray();
+    }
   }
 
   /*
@@ -65,8 +84,13 @@ public class InputStreamRequestBody extends RequestBody {
   @Override
   public void writeTo(BufferedSink sink) throws IOException {
     Source source = null;
+
     try {
-      source = Okio.source(inputStream);
+      if (bytes != null) {
+        source = Okio.source(new ByteArrayInputStream(bytes));
+      } else {
+        source = Okio.source(inputStream);
+      }
       sink.writeAll(source);
     } finally {
       Util.closeQuietly(source);
