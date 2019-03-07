997f38629040be6448ae78c8d5c3c59706fd13b2
Logan Patino <loganpatino10@gmail.com>
2019-03-07 15:47:16 -0500

    test(common): Add test for getDefaultHeaders()

--- common/src/test/java/com/ibm/watson/common/SdkCommonTest.java
+++ common/src/test/java/com/ibm/watson/common/SdkCommonTest.java
@@ -1,4 +1,27 @@
 package com.ibm.watson.common;
 
-public class SdkCommonTest {
+import com.ibm.cloud.sdk.core.http.HttpHeaders;
+import com.ibm.cloud.sdk.core.test.WatsonServiceUnitTest;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+public class SdkCommonTest extends WatsonServiceUnitTest {
+  @Test
+  public void testGetDefaultHeaders() {
+    String serviceName = "test_name";
+    String serviceVersion = "v1";
+    String operationId = "test_method";
+    Map<String, String> defaultHeaders = SdkCommon.getDefaultHeaders(serviceName, serviceVersion, operationId);
+
+    assertTrue(defaultHeaders.containsKey(HttpHeaders.X_IBMCLOUD_SDK_ANALYTICS));
+    String analyticsHeaderValue = defaultHeaders.get(HttpHeaders.X_IBMCLOUD_SDK_ANALYTICS);
+    assertTrue(analyticsHeaderValue.contains(serviceName));
+    assertTrue(analyticsHeaderValue.contains(serviceVersion));
+    assertTrue(analyticsHeaderValue.contains(operationId));
+    assertTrue(defaultHeaders.containsKey(HttpHeaders.USER_AGENT));
+    assertTrue(defaultHeaders.get(HttpHeaders.USER_AGENT).startsWith("watson-apis-java-sdk/"));
+  }
 }
