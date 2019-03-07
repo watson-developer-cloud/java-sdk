758f3f25b472d0d346352ffa76c15fcf5d152178
Logan Patino <loganpatino10@gmail.com>
2019-03-07 15:46:34 -0500

    feat(common): Retrieve User-Agent from core and append to current package version for full value

---
+++ common/src/main/java/com/ibm/watson/common/SdkCommon.java
@@ -0,0 +1,48 @@
+package com.ibm.watson.common;
+
+import com.ibm.cloud.sdk.core.http.HttpHeaders;
+import com.ibm.cloud.sdk.core.util.RequestUtils;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SdkCommon {
+  private static final Logger LOG = Logger.getLogger(SdkCommon.class.getName());
+
+  private static String loadSdkVersion() {
+    ClassLoader classLoader = SdkCommon.class.getClassLoader();
+    InputStream inputStream = classLoader.getResourceAsStream("java-sdk-version.properties");
+    Properties properties = new Properties();
+
+    try {
+      properties.load(inputStream);
+    } catch (Exception e) {
+      LOG.log(Level.WARNING, "Could not load java-sdk-version.properties", e);
+    }
+
+    return properties.getProperty("version", "unknown-version");
+  }
+
+  private static String getUserAgent() {
+    return "watson-apis-java-sdk/" + loadSdkVersion() + "; " + RequestUtils.getUserAgent();
+  }
+
+  public static Map<String, String> getDefaultHeaders(String serviceName, String serviceVersion, String operationId) {
+    Map<String, String> headers = new HashMap<>();
+
+    String sdkAnalyticsHeaderValue = String.format(
+        "service_name=%s;service_version=%s;operation_id=%s",
+        serviceName,
+        serviceVersion,
+        operationId
+    );
+
+    headers.put(HttpHeaders.X_IBMCLOUD_SDK_ANALYTICS, sdkAnalyticsHeaderValue);
+    headers.put(HttpHeaders.USER_AGENT, getUserAgent());
+    return headers;
+  }
+}
