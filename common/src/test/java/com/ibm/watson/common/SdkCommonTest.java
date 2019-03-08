40a845df2f2bc2987b320456c3a9136cc5d974f3
Logan Patino <loganpatino10@gmail.com>
2019-03-08 10:25:14 -0500

    test(SdkCommon): Remove unnecessary extension and change header class reference

--- common/src/test/java/com/ibm/watson/common/SdkCommonTest.java
+++ common/src/test/java/com/ibm/watson/common/SdkCommonTest.java
@@ -1,14 +1,13 @@
 package com.ibm.watson.common;
 
 import com.ibm.cloud.sdk.core.http.HttpHeaders;
-import com.ibm.cloud.sdk.core.test.WatsonServiceUnitTest;
 import org.junit.Test;
 
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
 
-public class SdkCommonTest extends WatsonServiceUnitTest {
+public class SdkCommonTest {
   @Test
   public void testGetDefaultHeaders() {
     String serviceName = "test_name";
@@ -16,8 +15,8 @@ public class SdkCommonTest extends WatsonServiceUnitTest {
     String operationId = "test_method";
     Map<String, String> defaultHeaders = SdkCommon.getDefaultHeaders(serviceName, serviceVersion, operationId);
 
-    assertTrue(defaultHeaders.containsKey(HttpHeaders.X_IBMCLOUD_SDK_ANALYTICS));
-    String analyticsHeaderValue = defaultHeaders.get(HttpHeaders.X_IBMCLOUD_SDK_ANALYTICS);
+    assertTrue(defaultHeaders.containsKey(WatsonHttpHeaders.X_IBMCLOUD_SDK_ANALYTICS));
+    String analyticsHeaderValue = defaultHeaders.get(WatsonHttpHeaders.X_IBMCLOUD_SDK_ANALYTICS);
     assertTrue(analyticsHeaderValue.contains(serviceName));
     assertTrue(analyticsHeaderValue.contains(serviceVersion));
     assertTrue(analyticsHeaderValue.contains(operationId));
