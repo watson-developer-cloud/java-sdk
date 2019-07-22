1a3d83b3b88a4422a353a9c15545736d3cdede38
Logan Patino <loganpatino10@gmail.com>
2019-07-22 11:48:23 -0400

    feat(Speech to Text): Add updated prop to AcousticModel and LanguageModel

--- speech-to-text/src/main/java/com/ibm/watson/speech_to_text/v1/model/LanguageModel.java
+++ speech-to-text/src/main/java/com/ibm/watson/speech_to_text/v1/model/LanguageModel.java
@@ -51,6 +51,7 @@ public class LanguageModel extends GenericModel {
   @SerializedName("customization_id")
   private String customizationId;
   private String created;
+  private String updated;
   private String language;
   private String dialect;
   private List<String> versions;
@@ -86,6 +87,19 @@ public class LanguageModel extends GenericModel {
    */
   public String getCreated() {
     return created;
+  }
+
+  /**
+   * Gets the updated.
+   *
+   * The date and time in Coordinated Universal Time (UTC) at which the custom language model was last modified. The
+   * `created` and `updated` fields are equal when a language model is first added but has yet to be updated. The value
+   * is provided in full ISO 8601 format (YYYY-MM-DDThh:mm:ss.sTZD).
+   *
+   * @return the updated
+   */
+  public String getUpdated() {
+    return updated;
   }
 
   /**
