133db0ecd31babb169e43a9c55434c4c2a718636
Logan Patino <loganpatino10@gmail.com>
2019-07-23 15:02:42 -0400

    feat(Compare and Comply): Add text and provenance ID info to various models

--- compare-comply/src/main/java/com/ibm/watson/compare_comply/v1/model/ContractAmts.java
+++ compare-comply/src/main/java/com/ibm/watson/compare_comply/v1/model/ContractAmts.java
@@ -12,6 +12,8 @@
  */
 package com.ibm.watson.compare_comply.v1.model;
 
+import java.util.List;
+
 import com.google.gson.annotations.SerializedName;
 import com.ibm.cloud.sdk.core.service.model.GenericModel;
 
@@ -32,10 +34,26 @@ public class ContractAmts extends GenericModel {
     String LOW = "Low";
   }
 
-  private String text;
   @SerializedName("confidence_level")
   private String confidenceLevel;
+  private String text;
+  @SerializedName("text_normalized")
+  private String textNormalized;
+  private Interpretation interpretation;
+  @SerializedName("provenance_ids")
+  private List<String> provenanceIds;
   private Location location;
+
+  /**
+   * Gets the confidenceLevel.
+   *
+   * The confidence level in the identification of the contract amount.
+   *
+   * @return the confidenceLevel
+   */
+  public String getConfidenceLevel() {
+    return confidenceLevel;
+  }
 
   /**
    * Gets the text.
@@ -49,14 +67,38 @@ public class ContractAmts extends GenericModel {
   }
 
   /**
-   * Gets the confidenceLevel.
+   * Gets the textNormalized.
    *
-   * The confidence level in the identification of the contract amount.
+   * The normalized form of the amount, which is listed as a string. This element is optional; that is, the service
+   * output lists it only if normalized text exists.
    *
-   * @return the confidenceLevel
+   * @return the textNormalized
    */
-  public String getConfidenceLevel() {
-    return confidenceLevel;
+  public String getTextNormalized() {
+    return textNormalized;
+  }
+
+  /**
+   * Gets the interpretation.
+   *
+   * The details of the normalized text, if applicable. This element is optional; that is, the service output lists it
+   * only if normalized text exists.
+   *
+   * @return the interpretation
+   */
+  public Interpretation getInterpretation() {
+    return interpretation;
+  }
+
+  /**
+   * Gets the provenanceIds.
+   *
+   * One or more hash values that you can send to IBM to provide feedback or receive support.
+   *
+   * @return the provenanceIds
+   */
+  public List<String> getProvenanceIds() {
+    return provenanceIds;
   }
 
   /**
