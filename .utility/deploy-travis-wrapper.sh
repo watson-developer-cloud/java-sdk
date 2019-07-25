0081be120c42b5010bbeda0c1673facbc1c12b52
Logan Patino <loganpatino10@gmail.com>
2019-07-25 12:07:16 -0400

    refactor: Use preferred command substitution syntax

--- .utility/deploy-travis-wrapper.sh
+++ .utility/deploy-travis-wrapper.sh
@@ -4,7 +4,7 @@
 
 # output every 9 min to prevent a Travis timeout
 PID=$!
-while [[ `ps -p $PID | tail -n +2` ]]; do
+while [[ $(ps -p $PID | tail -n +2) ]]; do
   echo 'Deploying...'
   sleep 540
 done
