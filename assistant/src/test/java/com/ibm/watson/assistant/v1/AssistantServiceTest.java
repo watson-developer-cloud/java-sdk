179ed46b369dfb003e56a43828f2ede99920cccc
Logan Patino <loganpatino10@gmail.com>
2019-08-28 11:38:39 -0400

    test(Assistant): Update tests

--- assistant/src/test/java/com/ibm/watson/assistant/v1/AssistantServiceTest.java
+++ assistant/src/test/java/com/ibm/watson/assistant/v1/AssistantServiceTest.java
@@ -12,7 +12,7 @@
  */
 package com.ibm.watson.assistant.v1;
 
-import com.ibm.cloud.sdk.core.service.security.IamOptions;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.common.WatsonServiceTest;
 import org.junit.Assume;
 import org.junit.Before;
@@ -45,12 +45,9 @@ public class AssistantServiceTest extends WatsonServiceTest {
 
     Assume.assumeFalse("config.properties doesn't have valid credentials.", apiKey == null);
 
-    service = new Assistant("2018-07-10");
+    IamAuthenticator authenticator = new IamAuthenticator(apiKey);
+    service = new Assistant("2018-07-10", authenticator);
     service.setEndPoint(getProperty("assistant.url"));
-    IamOptions iamOptions = new IamOptions.Builder()
-        .apiKey(apiKey)
-        .build();
-    service.setIamCredentials(iamOptions);
     service.setDefaultHeaders(getDefaultHeaders());
   }
 
--- assistant/src/test/java/com/ibm/watson/assistant/v2/AssistantServiceTest.java
+++ assistant/src/test/java/com/ibm/watson/assistant/v2/AssistantServiceTest.java
@@ -12,7 +12,7 @@
  */
 package com.ibm.watson.assistant.v2;
 
-import com.ibm.cloud.sdk.core.service.security.IamOptions;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.common.WatsonServiceTest;
 import org.junit.Assume;
 import org.junit.Before;
@@ -45,12 +45,9 @@ public class AssistantServiceTest extends WatsonServiceTest {
 
     Assume.assumeFalse("config.properties doesn't have valid credentials.", apiKey == null);
 
-    service = new Assistant("2019-02-28");
+    IamAuthenticator authenticator = new IamAuthenticator(apiKey);
+    service = new Assistant("2019-02-28", authenticator);
     service.setEndPoint(getProperty("assistant.url"));
-    IamOptions iamOptions = new IamOptions.Builder()
-        .apiKey(apiKey)
-        .build();
-    service.setIamCredentials(iamOptions);
     service.setDefaultHeaders(getDefaultHeaders());
   }
 
