42f6ec65b1536d578f7464822d94db7639a9df81
Logan Patino <loganpatino10@gmail.com>
2020-01-14 14:42:42 -0500

    feat(Speech to Text): Add endOfUtterance prop to SpeechRecognitionResult

--- speech-to-text/src/main/java/com/ibm/watson/speech_to_text/v1/model/SpeechRecognitionResult.java
+++ speech-to-text/src/main/java/com/ibm/watson/speech_to_text/v1/model/SpeechRecognitionResult.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2019.
+ * (C) Copyright IBM Corp. 2020.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -23,13 +23,35 @@ import com.ibm.cloud.sdk.core.service.model.GenericModel;
  */
 public class SpeechRecognitionResult extends GenericModel {
 
+  /**
+   * If the `split_transcript_at_phrase_end` parameter is `true`, describes the reason for the split:
+   * * `end_of_data` - The end of the input audio stream.
+   * * `full_stop` - A full semantic stop, such as for the conclusion of a grammatical sentence. The insertion of splits
+   * is influenced by the base language model and biased by custom language models and grammars.
+   * * `reset` - The amount of audio that is currently being processed exceeds the two-minute maximum. The service
+   * splits the transcript to avoid excessive memory use.
+   * * `silence` - A pause or silence that is at least as long as the pause interval.
+   */
+  public interface EndOfUtterance {
+    /** end_of_data. */
+    String END_OF_DATA = "end_of_data";
+    /** full_stop. */
+    String FULL_STOP = "full_stop";
+    /** reset. */
+    String RESET = "reset";
+    /** silence. */
+    String SILENCE = "silence";
+  }
+
   @SerializedName("final")
-  private Boolean xFinal;
-  private List<SpeechRecognitionAlternative> alternatives;
+  protected Boolean xFinal;
+  protected List<SpeechRecognitionAlternative> alternatives;
   @SerializedName("keywords_result")
-  private Map<String, List<KeywordResult>> keywordsResult;
+  protected Map<String, List<KeywordResult>> keywordsResult;
   @SerializedName("word_alternatives")
-  private List<WordAlternativeResults> wordAlternatives;
+  protected List<WordAlternativeResults> wordAlternatives;
+  @SerializedName("end_of_utterance")
+  protected String endOfUtterance;
 
   /**
    * Gets the xFinal.
@@ -79,5 +101,22 @@ public class SpeechRecognitionResult extends GenericModel {
    */
   public List<WordAlternativeResults> getWordAlternatives() {
     return wordAlternatives;
+  }
+
+  /**
+   * Gets the endOfUtterance.
+   *
+   * If the `split_transcript_at_phrase_end` parameter is `true`, describes the reason for the split:
+   * * `end_of_data` - The end of the input audio stream.
+   * * `full_stop` - A full semantic stop, such as for the conclusion of a grammatical sentence. The insertion of splits
+   * is influenced by the base language model and biased by custom language models and grammars.
+   * * `reset` - The amount of audio that is currently being processed exceeds the two-minute maximum. The service
+   * splits the transcript to avoid excessive memory use.
+   * * `silence` - A pause or silence that is at least as long as the pause interval.
+   *
+   * @return the endOfUtterance
+   */
+  public String getEndOfUtterance() {
+    return endOfUtterance;
   }
 }
