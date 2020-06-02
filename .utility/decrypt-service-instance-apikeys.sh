f2f4846d5a48390fbcb3b3d9f863ce90993b6b0b
German Attanasio <germanattanasio@gmail.com>
2020-06-02 12:07:14 -0400

    test(travis): add encrypted credentials

--- .utility/decrypt-service-instance-apikeys.sh
+++ .utility/decrypt-service-instance-apikeys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Decrypt service instance apikeys for branch $TRAVIS_BRANCH.\n"
-  openssl aes-256-cbc -K $encrypted_42d9c68e608d_key -iv $encrypted_42d9c68e608d_iv -in secrets.tar.enc -out secrets.tar -d
-  tar xvf secrets.tar
+  openssl aes-256-cbc -K $encrypted_75da58c42a01_key -iv $encrypted_75da58c42a01_iv \
+  -in config.properties.enc -out common/src/test/resources/config.properties -d
 else
   echo -e "Not Decrypt service instance apikeys for branch $TRAVIS_BRANCH.\n"
 fi
