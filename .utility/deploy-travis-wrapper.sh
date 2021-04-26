3d944aae36a2ae1aac91be46b49ebeaa010312d2
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-04-26 15:38:28 -0500

    ci(gradle): add sigining key

--- .utility/deploy-travis-wrapper.sh
+++ .utility/deploy-travis-wrapper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -o pipefail
 
-./gradlew uploadArchives &
+openssl aes-256-cbc -K $encrypted_99a33d910092_key -iv $encrypted_99a33d910092_iv -in .utility/secring.gpg.enc -out .utility/secring.gpg -d
+./gradlew uploadArchives -Psigning.keyId=$SIGNING_KEY -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=./scripts/secring.gpg &
 
 # output every 9 min to prevent a Travis timeout
 PID=$!
