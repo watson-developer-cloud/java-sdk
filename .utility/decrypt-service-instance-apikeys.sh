f7d75d31ae1546e54032d727f8d2457cf4dc6d4d
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-04-26 13:30:37 -0500

    ci(decrypt-service-instance-apikeys): remove generated github lines

--- .utility/decrypt-service-instance-apikeys.sh
+++ .utility/decrypt-service-instance-apikeys.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Decrypt service instance apikeys for branch $TRAVIS_BRANCH.\n"
-<<<<<<< HEAD
   openssl aes-256-cbc -K $encrypted_b3aab48b571a_key -iv $encrypted_b3aab48b571a_iv \
-=======
-  openssl aes-256-cbc -K $encrypted_5b4197fb87e6_key -iv $encrypted_5b4197fb87e6_iv \
->>>>>>> master
   -in config.properties.enc -out common/src/test/resources/config.properties -d
 else
   echo -e "Not Decrypt service instance apikeys for branch $TRAVIS_BRANCH.\n"
