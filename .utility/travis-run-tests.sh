70b9084a28eda59f0ac10157b90dd361afcd74a7
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-05-04 16:28:37 -0500

    chore(gradle): remove scripts used for gradle deployment

--- .utility/travis-run-tests.sh
+++
@@ -1,8 +0,0 @@
-set -o pipefail
-
-if [ "${TRAVIS_EVENT_TYPE}" == "push"  ] && [ "${TRAVIS_BRANCH}" != "master" ] && [ "${TRAVIS_TAG}" != "${TRAVIS_BRANCH}" ]; then
-  if test -f "common/src/test/resources/config.properties"; then
-    echo "Running integration tests"
-  fi
-  ./gradlew codeCoverageReport --continue
-fi
