9e5ec26aea3a4560bb46b49c6487df31d4da8fa5
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-05-17 11:39:26 -0500

    chore(javadoc): update to have the same content it had before moving into maven

--- build/generateJavadocIndex.sh
+++ build/generateJavadocIndex.sh
@@ -8,17 +8,19 @@ echo '<!DOCTYPE html>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>IBM Watson Java SDK </title>
+    <title>IBM Watson Developer Cloud</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
     <div class="page-header">
-        <h1>Java client library to use the IBM Watson APIs</h1>
+        <h1>IBM Watson Developer Cloud Java SDK</h1>
     </div>
 
-    <p><a href="https://cloud.ibm.com/apidocs?category=<service-category>">My Services Info</a>
+    <p><a href="https://www.ibm.com/watson/developer/">Info</a>
+        | <a href="https://cloud.ibm.com/developer/watson/documentation">Documentation</a>
         | <a href="https://github.com/watson-developer-cloud/java-sdk">GitHub</a>
+        | <a href="https://search.maven.org/search?q=g:com.ibm.watson">Maven</a>
     </p>
 
     <p>Javadoc by branch/release:</p>
