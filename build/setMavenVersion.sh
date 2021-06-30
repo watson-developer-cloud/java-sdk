9874d4dd6c2a20dadebdb71c90e73c9ce91f718b
Michael G Mosca <moscam@us.ibm.com>
2021-06-30 12:53:02 -0400

    ci: fix

--- build/setMavenVersion.sh
+++ build/setMavenVersion.sh
@@ -2,6 +2,7 @@
 
 # This script will check $TRAVIS_TAG to see if we need to run maven to
 # set the artifact version #'s.
+printf "\n>>>>> WWW Setting artifact version #'s to: %s\n" ${TRAVIS_TAG:1}
 
 if [[ -n "${TRAVIS_TAG}" ]]; then
     printf "\n>>>>> Setting artifact version #'s to: %s\n" ${TRAVIS_TAG:1}
