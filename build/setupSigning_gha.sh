d45b140db70f8825a9ab3038397358d67c1341d4
Michael G Mosca <moscam@us.ibm.com>
2021-08-26 15:14:16 -0400

    ci: fix gpg

---
+++ build/setupSigning_gha.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -x
+
+# This script is responsible for decrypting your encrypted signing key file
+# (build/signing.key.enc), and importing it into the gpg keystore.
+# This is done so that your maven build will be able to properly sign your jars
+# prior to publishing them on maven central.
+
+echo "Importing signing key..."
+
+# Modify the command below to use the correct environment variables
+# that were added to your Travis build settings when you encrypted your signing.key file.
+gpg --quiet --batch --yes --decrypt --passphrase="$SIGNING_PASSPHRASE" --output ./build/signing.key ./build/signing.key.gpg
+
+gpg --version
+gpg --fast-import build/signing.key
+rm build/signing.key
+
+echo "Signing key import finished!"
