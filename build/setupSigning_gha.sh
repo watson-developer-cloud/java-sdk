c521f079fdd6c3950d841aaa082807a62cfecfb6
Michael G Mosca <moscam@us.ibm.com>
2021-08-27 11:25:59 -0400

    ci: fix

--- build/setupSigning_gha.sh
+++ build/setupSigning_gha.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export GPG_TTY=$(tty)
+
 set -x
 
 # This script is responsible for decrypting your encrypted signing key file
@@ -12,7 +14,6 @@ echo "Importing signing key..."
 # that were added to your Travis build settings when you encrypted your signing.key file.
 gpg --quiet --batch --yes --decrypt --passphrase="$SIGNING_PASSPHRASE" --output ./build/signing.key ./build/signing.key.gpg
 
-# export GPG_TTY=$(tty)
 gpg --version
 gpg --fast-import ./build/signing.key
 rm ./build/signing.key
