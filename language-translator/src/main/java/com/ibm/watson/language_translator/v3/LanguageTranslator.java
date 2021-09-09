7d2f9edff6ca8e6b615e52fa865a959462cb6e43
Kevin Kowalski <kevin.kowalski@ibm.com>
2021-09-09 02:33:56 -0500

    docs(language-translator-v3): documentation updates

--- language-translator/src/main/java/com/ibm/watson/language_translator/v3/LanguageTranslator.java
+++ language-translator/src/main/java/com/ibm/watson/language_translator/v3/LanguageTranslator.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2019, 2020.
+ * (C) Copyright IBM Corp. 2021.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -12,7 +12,7 @@
  */
 
 /*
- * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-be3b4618-20201201-123423
+ * IBM OpenAPI SDK Code Generator Version: 3.38.0-07189efd-20210827-205025
  */
 
 package com.ibm.watson.language_translator.v3;
@@ -61,8 +61,7 @@ import okhttp3.MultipartBody;
  * unique terminology and language. Use Language Translator to take news from across the globe and
  * present it in your language, communicate with your customers in their own language, and more.
  *
- * @version v3
- * @see <a href="https://cloud.ibm.com/docs/language-translator">Language Translator</a>
+ * <p>API Version: 3.0.0 See: https://cloud.ibm.com/docs/language-translator
  */
 public class LanguageTranslator extends BaseService {
 
