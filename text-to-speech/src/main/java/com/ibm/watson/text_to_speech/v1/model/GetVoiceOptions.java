40cd0358c131d2e3767b16d9bedf2ddd256e7fd2
Kevin Kowalski <kevin.kowalski@ibm.com>
2022-03-15 15:41:48 -0500

    feat(text-to-speech-v1): add voices and update comments

--- text-to-speech/src/main/java/com/ibm/watson/text_to_speech/v1/model/GetVoiceOptions.java
+++ text-to-speech/src/main/java/com/ibm/watson/text_to_speech/v1/model/GetVoiceOptions.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2018, 2021.
+ * (C) Copyright IBM Corp. 2022.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -17,18 +17,14 @@ import com.ibm.cloud.sdk.core.service.model.GenericModel;
 /** The getVoice options. */
 public class GetVoiceOptions extends GenericModel {
 
-  /**
-   * The voice for which information is to be returned. For more information about specifying a
-   * voice, see **Important voice updates for IBM Cloud** in the method description.
-   *
-   * <p>**IBM Cloud:** The Arabic, Chinese, Dutch, Australian English, and Korean languages and
-   * voices are supported only for IBM Cloud.
-   */
+  /** The voice for which information is to be returned. */
   public interface Voice {
     /** ar-AR_OmarVoice. */
     String AR_AR_OMARVOICE = "ar-AR_OmarVoice";
     /** ar-MS_OmarVoice. */
     String AR_MS_OMARVOICE = "ar-MS_OmarVoice";
+    /** cs-CZ_AlenaVoice. */
+    String CS_CZ_ALENAVOICE = "cs-CZ_AlenaVoice";
     /** de-DE_BirgitVoice. */
     String DE_DE_BIRGITVOICE = "de-DE_BirgitVoice";
     /** de-DE_BirgitV3Voice. */
@@ -39,10 +35,12 @@ public class GetVoiceOptions extends GenericModel {
     String DE_DE_DIETERV3VOICE = "de-DE_DieterV3Voice";
     /** de-DE_ErikaV3Voice. */
     String DE_DE_ERIKAV3VOICE = "de-DE_ErikaV3Voice";
-    /** en-AU-CraigVoice. */
-    String EN_AU_CRAIGVOICE = "en-AU-CraigVoice";
-    /** en-AU-MadisonVoice. */
-    String EN_AU_MADISONVOICE = "en-AU-MadisonVoice";
+    /** en-AU_CraigVoice. */
+    String EN_AU_CRAIGVOICE = "en-AU_CraigVoice";
+    /** en-AU_MadisonVoice. */
+    String EN_AU_MADISONVOICE = "en-AU_MadisonVoice";
+    /** en-AU_SteveVoice. */
+    String EN_AU_STEVEVOICE = "en-AU_SteveVoice";
     /** en-GB_CharlotteV3Voice. */
     String EN_GB_CHARLOTTEV3VOICE = "en-GB_CharlotteV3Voice";
     /** en-GB_JamesV3Voice. */
@@ -113,6 +111,8 @@ public class GetVoiceOptions extends GenericModel {
     String KO_KR_YUNAVOICE = "ko-KR_YunaVoice";
     /** nl-BE_AdeleVoice. */
     String NL_BE_ADELEVOICE = "nl-BE_AdeleVoice";
+    /** nl-BE_BramVoice. */
+    String NL_BE_BRAMVOICE = "nl-BE_BramVoice";
     /** nl-NL_EmmaVoice. */
     String NL_NL_EMMAVOICE = "nl-NL_EmmaVoice";
     /** nl-NL_LiamVoice. */
@@ -121,6 +121,8 @@ public class GetVoiceOptions extends GenericModel {
     String PT_BR_ISABELAVOICE = "pt-BR_IsabelaVoice";
     /** pt-BR_IsabelaV3Voice. */
     String PT_BR_ISABELAV3VOICE = "pt-BR_IsabelaV3Voice";
+    /** sv-SE_IngridVoice. */
+    String SV_SE_INGRIDVOICE = "sv-SE_IngridVoice";
     /** zh-CN_LiNaVoice. */
     String ZH_CN_LINAVOICE = "zh-CN_LiNaVoice";
     /** zh-CN_WangWeiVoice. */
@@ -204,11 +206,7 @@ public class GetVoiceOptions extends GenericModel {
   /**
    * Gets the voice.
    *
-   * <p>The voice for which information is to be returned. For more information about specifying a
-   * voice, see **Important voice updates for IBM Cloud** in the method description.
-   *
-   * <p>**IBM Cloud:** The Arabic, Chinese, Dutch, Australian English, and Korean languages and
-   * voices are supported only for IBM Cloud.
+   * <p>The voice for which information is to be returned.
    *
    * @return the voice
    */
