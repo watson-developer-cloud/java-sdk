ed13391db2ddbf9e27f27ef14ceefd19e88be3f1
Angelo Paparazzi <Angelo.Paparazzi@ibm.com>
2022-08-02 15:07:17 -0500

    test(assistantv2): update unit tests for new methods

---
+++ assistant/src/test/java/com/ibm/watson/assistant/v2/model/GetReleaseOptionsTest.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright IBM Corp. 2022.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.ibm.watson.assistant.v2.model;
+
+import static org.testng.Assert.*;
+
+import com.ibm.cloud.sdk.core.service.model.FileWithMetadata;
+import com.ibm.watson.assistant.v2.utils.TestUtilities;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import org.testng.annotations.Test;
+
+/** Unit test class for the GetReleaseOptions model. */
+public class GetReleaseOptionsTest {
+  final HashMap<String, InputStream> mockStreamMap = TestUtilities.createMockStreamMap();
+  final List<FileWithMetadata> mockListFileWithMetadata =
+      TestUtilities.creatMockListFileWithMetadata();
+
+  @Test
+  public void testGetReleaseOptions() throws Throwable {
+    GetReleaseOptions getReleaseOptionsModel =
+        new GetReleaseOptions.Builder()
+            .assistantId("testString")
+            .release("testString")
+            .includeAudit(false)
+            .build();
+    assertEquals(getReleaseOptionsModel.assistantId(), "testString");
+    assertEquals(getReleaseOptionsModel.release(), "testString");
+    assertEquals(getReleaseOptionsModel.includeAudit(), Boolean.valueOf(false));
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testGetReleaseOptionsError() throws Throwable {
+    new GetReleaseOptions.Builder().build();
+  }
+}
